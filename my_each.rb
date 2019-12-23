c5ced7bf699d66e87150eb8e379ccc735adfd930
Abraham Galva <ag212@icloud.com>
2019-12-23 20:04:52 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
-  # code here
+def my_each (words)
+  words = ["arel", "jon", "logan", "spencer"]
+  words.each do |words|
+    puts "it's almost ready!"
+    yield(words)
+    puts "I Love #{words}!"
+  end
 end
+
